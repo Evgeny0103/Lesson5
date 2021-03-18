5e751328fdc5aec6d6d6333fcbf6e370eba96ab2
Evgeny0103 <38032854+Evgeny0103@users.noreply.github.com>
2021-03-18 20:58:09 +0600

    Lesson5
    Lesson5

---
+++ AlgoritmLesson5/AlgoritmLesson5/BTree.cs
@@ -0,0 +1,438 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoritmLesson5
+{
+    public class BTree
+    {
+        /// <summary>Задержка для визуализации алгоритма</summary>
+        private const int DELAY = 250;
+
+        #region ---- PROPERTIES
+
+        /// <summary>Корень дерева</summary>
+        public Node Root
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>Количество узлов в дереве</summary>
+        public int Count
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region ---- ADDITION METHODS ----
+
+        /// <summary>Добавляет новый узел</summary>
+        /// <param name="value">Значение хранимое в узле</param>
+        public void AddNode(int value)
+        {
+            // 1 вариант:  Дерево пустое - создаем корень дерева
+            if (Root == null)
+                Root = new Node(value, null, this);
+            // 2 вариант: Дерево не пустое - ищем место для добавления нового узла
+            else
+            {
+                //AddToNode(Root, value);
+                Node current = Root;//Текущий узел
+
+                bool isDone = false;//Флаг окончания процесса вставки
+                while (!isDone)//Проверяем пока не дойдем до пустого узла
+                {
+                    Node curent = Root;
+                    int check = current.CompareTo(value);
+                    if (check > 0)//Если вставляемое значение меньше то влево
+                    {
+                        if (current.Left != null)
+                        {
+                            current = current.Left;
+                        }
+                        else
+                        {
+                            current.Left = new Node(value, current, this);
+                            Count++;
+                            current.Balance();
+                            isDone = true;
+                        }
+                    }
+                    else if (check < 0)//Если вставляемое значение больше то вправо
+                    {
+                        if (current.Right != null)
+                        {
+                            current = current.Right;
+                        }
+                        else
+                        {
+                            current.Right = new Node(value, current, this);
+                            Count++;
+                            current.Balance();
+                            isDone = true;
+                        }
+                    }
+                    else//Если совпадает, то заносить в дерево не будем (дубликаты не нужны)
+                        isDone = true;
+                }
+
+            }
+
+
+        }
+
+        #endregion
+
+        #region ---- SEARCH METHODS ----
+
+        /// <summary>Проверяет содержит ли дерево заданное значение</summary>
+        /// <param name="value">Искомое значение</param>
+        /// <returns>true, если узел с таким значением есть в дереве</returns>
+        public bool Contains(int value)
+        {
+            return Find(value) != null;
+        }
+
+        /// <summary> 
+        /// Находит и возвращает узел который содержит искомое значение.
+        /// Если значение не найдено, возвращает null. 
+        /// Так же возвращает родительский узел.
+        /// </summary> /// 
+        /// <param name="value">Значение поиска</param> 
+        /// <param name="parent">Родительский элемент для найденного значения/// </param> 
+        /// <returns> Найденный узел (null, если узел не найден) /// </returns> 
+        private Node Find(int value)
+        {
+            // Указатель на текущий узел. Начинаем с корня дерева
+            Node current = Root;
+
+            // Пока текщий узел на пустой 
+            while (current != null)
+            {
+                int result = current.CompareTo(value);
+
+                // Если значение меньшне текущего - переход влево 
+                if (result > 0)
+                    current = current.Left;
+                // Если значение больше текщего - переход вправо
+                else if (result < 0)
+                    current = current.Right;
+                // Если значение совпадает - узел найден
+                else
+                    break;
+            }
+            return current;
+        }
+
+        /// <summary>Возвращает количество узлов в дереве</summary>
+        /// <returns>Количество узлов в дереве</returns>
+        public int GetCount()
+        {
+            return Count;
+        }
+
+        #endregion
+
+        #region ---- NEW SEARCH METHODS ----
+
+        /// <summary>
+        /// Метод бинарного поиска с задержкой и раскраской для визуализации алгоритма
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <returns>true, если узел с таким значением есть в дереве</returns>
+        public bool BinarySearch(int value)
+        {
+            // Указатель на текущий узел. Начинаем с корня дерева
+            Node current = Root;
+
+            // Пока текщий узел на пустой 
+            while (current != null)
+            {
+                int result = current.CompareTo(value);
+                ColorPrint();
+
+                // Если значение меньшне текущего - переход влево 
+                if (result > 0)
+                {
+                    current.Color = ConsoleColor.Red;
+                    current = current.Left;
+                }
+                // Если значение больше текщего - переход вправо
+                else if (result < 0)
+                {
+                    current.Color = ConsoleColor.Red;
+                    current = current.Right;
+                }
+                // Если значение совпадает - узел найден
+                else
+                {
+                    current.Color = ConsoleColor.Green;
+                    break;
+                }
+            }
+
+            ColorPrint();
+
+            return current != null; ;
+        }
+
+        /// <summary>
+        /// Поиск (обход) дерева в ширину
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <returns>true, если узел с таким значением есть в дереве</returns>
+        public bool BFS(int value)
+        {
+            Queue<Node> bufer = new Queue<Node>();
+
+            bufer.Enqueue(this.Root);
+
+            bool isFound = false;
+            while (bufer.Count != 0 && !isFound)
+            {
+                Node element = bufer.Dequeue();
+                if (element.Value == value) isFound = true;
+                element.Color = isFound ? ConsoleColor.Green : ConsoleColor.Red;
+                ColorPrint();
+                if (element.Left != null) bufer.Enqueue(element.Left);
+                if (element.Right != null) bufer.Enqueue(element.Right);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+
+            return isFound;
+        }
+
+        /// <summary>
+        /// Поиск (обход) дерева в глубину
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <returns>true, если узел с таким значением есть в дереве</returns>
+        public bool DFS(int value)
+        {
+            Stack<Node> bufer = new Stack<Node>();
+
+            bufer.Push(this.Root);
+
+            bool isFound = false;
+            while (bufer.Count != 0 && !isFound)
+            {
+                Node element = bufer.Pop();
+                if (element.Value == value) isFound = true;
+                element.Color = isFound ? ConsoleColor.Green : ConsoleColor.Red;
+                ColorPrint();
+                if (element.Right != null) bufer.Push(element.Right);
+                if (element.Left != null) bufer.Push(element.Left);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+
+            return isFound;
+        }
+
+        #endregion
+
+        #region ---- REMOVE METHODS ----
+
+        /// <summary>Удаление элемента из дерева</summary>
+        /// <param name="value">Значение узла который нужно удалить из дерева</param>
+        /// <returns>true, если операция прошла успешно. false, если узел не найден.</returns>
+        public bool RemoveNode(int value)
+        {
+            Node current; //Указатель на удаляемый узел
+
+            //Проверяем есть ли узел с заданным значением в дереве
+            current = Find(value);
+
+            //Если не нашли, то выходим
+            if (current == null)
+                return false;
+
+            //Узел начиная с которого будем балансировать дереве после удаления
+            Node treeToBalance = current.Parent;
+            Count--;
+
+            // 1 вариант: Если у удаляемого узла нет правого потомка.
+            //
+            // До                 После
+            //     8                 8
+            //    / \               / \
+            //  (5)  9             2   9
+            //  /   / \               / \
+            // 2   10  16            10  16
+            //     
+
+
+            if (current.Right == null)
+            {
+                //Если удаляемый узел - корень дерева, то перемещаем левое поддерево, на его место.
+                if (current.Parent == null)
+                {
+                    Root = current.Left;
+                    //Убераем ссылку на родителя у корня
+                    if (Root != null)
+                        Root.Parent = null;
+                }
+                //Если удаляемый узел не является корнем дерева
+                else
+                {
+                    int result = current.Parent.CompareTo(current.Value);
+
+                    // Если значение родительского узла больше значения удаляемого,
+                    // делаем левого потомка удаляемого узла, левым потомком его родительского узла.  
+                    if (result > 0)
+                        current.Parent.Left = current.Left;
+                    // Если значение родительского узла меньше чем удаляемого,                 
+                    // делаем левого потомка удаляемого узла - правым потомком его родительского узла.
+                    else if (result < 0)
+                        current.Parent.Right = current.Left;
+                }
+            }
+
+            // 2 Вариант: Если у правого потомка удаляемого узла нет левого потомка, 
+            // тогда правый потомок удаляемого узла
+            // становится потомком родительского узла.      
+            //
+            // До                 После
+            //      8                  8
+            //    /   \               / \
+            //  (5)     9           6     9
+            //  / \    / \        / \     / \
+            // 2   6  10  16     2   7   10  16
+            //      \
+            //       7
+            //
+
+            // Если у правого потомка нет левого потомка
+            else if (current.Right.Left == null)
+            {
+                current.Right.Left = current.Left;
+
+                //Если удаляемый узел - корень дерева, то перемещаем правое поддерево, на его место.
+                if (current.Parent == null)
+                {
+                    Root = current.Right;
+                    //Убераем ссылку на родителя у корня
+                    if (Root != null)
+                        Root.Parent = null;
+                }
+                //Если удаляемый узел не является корнем дерева
+                else
+                {
+                    int result = current.Parent.CompareTo(current.Value);
+                    // Если значение родительского узла больше значения удаляемого,
+                    // делаем правого потомка удаляемого узла, левым потомком его родительского узла
+                    if (result > 0)
+                        current.Parent.Left = current.Right;
+                    // Если значение родительского узла меньше значения удаляемого,                 
+                    // делаем правого потомка удаляемого узла, правым потомком его родительского узла
+                    else if (result < 0)
+                        current.Parent.Right = current.Right;
+                }
+            }
+
+            // 3 вариант: Если правый потомок удаляемого узла имеет левого потомка,      
+            // заменяем удаляемый узел, на крайнего левого потомка правого потомка.     
+            //
+            // До                 После
+            //      8                  8
+            //    /   \               / \
+            //  (5)     9           6     9
+            //  / \    / \        / \     / \
+            // 2   7  10  16     2   7   10  16
+            //    / 
+            //   6
+            //
+
+            else
+            {
+                // Находим крайний левы узел для правого потомка удаляемого узла.
+                Node leftmost = current.Right.Left;
+
+                while (leftmost.Left != null)
+                    leftmost = leftmost.Left;
+
+                // Родительское правое поддерево становится родительским левым поддеревом.         
+                leftmost.Parent.Left = leftmost.Right;
+
+                // Присвоить крайнему левому узлу, ссылки на правого и левого потомка удаляемого узла.         
+                leftmost.Left = current.Left;
+                leftmost.Right = current.Right;
+
+
+                //Если удаляемый узел - корень дерева, то перемещаем крайний левый узел, на его место.
+                if (current.Parent == null)
+                {
+                    Root = leftmost;
+                    //Убераем ссылку на родителя у корня
+                    if (Root != null)
+                        Root.Parent = null;
+                }
+                //Если удаляемый узел не является корнем дерева
+                else
+                {
+                    int result = current.Parent.CompareTo(current.Value);
+
+                    // Если значение родительского узла больше значения удаляемого,                 
+                    // делаем крайнего левого потомка удаляемого узла, левым потомком его родительского узла
+                    if (result > 0)
+                        current.Parent.Left = leftmost;
+                    // Если значение родительского узла, меньше значения удаляемого,                 
+                    // делаем крайнего левого потомка удаляемого узла, правым потомком его родительского узла
+                    else if (result < 0)
+                        current.Parent.Right = leftmost;
+                }
+            }
+
+            //Если мы не в корне то балансируем дерево, начиная с родительского потомка удаленного узла
+            if (treeToBalance != null)
+                treeToBalance.Balance();
+            //Если в корне, то просто балансируем от корня.
+            else
+            {
+                //Балансируем только если дерево не пустое
+                if (Root != null)
+                    Root.Balance();
+            }
+
+            return true;
+        }
+
+        /// <summary>Очистка дерева</summary>
+        public void ClearTree()
+        {
+            Root = null;
+            Count = 0;
+        }
+
+        #endregion
+
+        #region ---- PRINT METHODS ----
+
+        /// <summary>
+        /// Выводит структуру дерева в консоль.
+        /// </summary>
+        public void Print()
+        {
+            if (Root != null)
+                Root.PrintNode("", Node.NodePosition.center, true, false);
+            else
+                Console.WriteLine("Tree is empty.");
+        }
+
+        /// <summary>
+        /// Вывод дерева на экран с раскраской узлов
+        /// </summary>
+        private void ColorPrint()
+        {
+            Console.Clear();
+            BTreePrinter.Print(Root, false);
+            System.Threading.Thread.Sleep(DELAY);//небольшая задержка для наглядности работы алгоритма
+        }
+
+        #endregion
+    }
+}
---
+++ AlgoritmLesson5/AlgoritmLesson5/Program.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoritmLesson5
+{
+    class Program
+    {
+
+        #region -- STRING CONSTS --
+
+        /// <summary> Ключи для словаря аргументов командной строки </summary>
+        enum Arguments
+        {
+            Help,
+            Seed,
+            SeedHelp
+        }
+
+        /// <summary> Словарь аргументов командной строки </summary>
+        private static readonly Dictionary<Arguments, string> arguments = new Dictionary<Arguments, string>
+        {
+        { Arguments.Help, "-h"},
+        { Arguments.Seed, "-s"},
+        { Arguments.SeedHelp, "-s <int> - seed for random number generator"},
+        };
+
+        /// <summary> Ключи для словаря с сообщениями об ошибках </summary>
+        enum Errors
+        {
+            ItemNotFound,
+            RepeatInputError,
+        }
+
+        /// <summary> Словарь с сообщениями об ошибках </summary>
+        private static readonly Dictionary<Errors, string> errors = new Dictionary<Errors, string>
+        {
+        { Errors.ItemNotFound, "Элемент не найден."},
+        { Errors.RepeatInputError, "Ошибка. Повторите ввод."}
+        };
+
+        /// <summary> Ключи для словаря с ссобщениями для пользователя </summary>
+        enum Messages
+        {
+            ChooseOption,
+            EnterNumber,
+            PressAnyKey,
+            From,
+            To,
+            NumbersList,
+            Amount,
+            Contain,
+            NotContain,
+            WhiteSpaceLine
+        }
+
+        /// <summary> Словарь с сообщениями для пользователя </summary>
+        private static readonly Dictionary<Messages, string> messages = new Dictionary<Messages, string>
+        {
+        { Messages.ChooseOption, "Выберите опцию:"},
+        { Messages.EnterNumber, "Введите число: "},
+        { Messages.PressAnyKey, "Нажмите любую клавишу."},
+        { Messages.From, "от"},
+        { Messages.To, "до"},
+        { Messages.NumbersList, "Содержимое дерева"},
+        { Messages.Amount, "всего"},
+        { Messages.Contain, "Данное число присутствует в дереве."},
+        { Messages.NotContain, "Данного числа нет в дереве."},
+        { Messages.WhiteSpaceLine, "        "}
+        };
+
+        /// <summary> Пункты главного меню, последний пункт выход из программы </summary>
+        private static readonly string[] mainMenu = new string[]
+        {
+            "Бинарный поиск",
+            "Поиск в ширину",
+            "Поиск в глубину\n",
+            "Выход"
+        };
+
+        #endregion
+
+        #region -- NUMBER CONSTS --
+
+        /// <summary>Минимальное значение числа хранимого в узле дерева</summary>
+        private const int VALUE_MIN = 0;
+        /// <summary>Максимальное значение числа хранимого в узле дерева</summary>
+        private const int VALUE_MAX = 99;
+        /// <summary>Количество узлов в дереве (для первоначального случайного заполнения)</summary>
+        private const int ELEMENTS = 20;
+
+        #endregion
+
+        /// <summary>Генератор случайных чисел</summary>
+        private static Random rnd;
+
+
+        static int Main(string[] args)
+        {
+            #region ---- INIT ----
+
+            int seed = 0;
+
+            //Обработка аругментов командной строки
+            if (args.Length != 0)
+            {
+                if (args[0] == arguments[Arguments.Help])//Вывод справки по аргументам
+                {
+                    Console.WriteLine(arguments[Arguments.SeedHelp]);
+                    return 0;
+                }
+                else if (args[0] == arguments[Arguments.Seed])//Изменение seed'a
+                {
+                    try
+                    {
+                        int.TryParse(args[1], out seed);
+                    }
+                    catch //Если аргумент не введен или введен неправильно, то будет полный рандом
+                    {
+                        seed = 0;
+                    }
+                }
+            }
+
+            if (seed != 0)
+                rnd = new Random(seed);
+            else
+                rnd = new Random();
+
+            //Формируем сообщение главного меню
+            string mainMenuMessage = "\n" + messages[Messages.ChooseOption] + "\n\n";
+            for (int i = 0; i < mainMenu.Length; i++)
+                mainMenuMessage += $"{i + 1} - {mainMenu[i]}\n";
+
+            #endregion
+
+            //Создаем дерево
+            BTree tree = new BTree();
+
+            for (int i = 0; i < ELEMENTS; i++)
+            {
+                AddRandomNumberToTree(tree, rnd);
+                Console.Clear();
+            }
+
+            bool printMethod = false;
+
+            Print(tree, printMethod);
+
+
+            //Основной цикл
+            bool isExit = false;
+            while (!isExit)
+            {
+                int input = NumberInput(mainMenuMessage, 1, mainMenu.Length);
+                switch (input)
+                {
+                    case 1://binary search
+                        Print(tree, printMethod);
+                        bool isContain = tree.BinarySearch(NumberInput(messages[Messages.EnterNumber], VALUE_MIN, VALUE_MAX, false));
+                        MessageWaitKey(isContain ? messages[Messages.Contain] : messages[Messages.NotContain]);
+                        Print(tree, printMethod);
+                        break;
+                    case 2://BFS
+                        Print(tree, printMethod);
+                        isContain = tree.BFS(NumberInput(messages[Messages.EnterNumber], VALUE_MIN, VALUE_MAX, false));
+                        MessageWaitKey(isContain ? messages[Messages.Contain] : messages[Messages.NotContain]);
+                        Print(tree, printMethod);
+                        break;
+                    case 3://DFS
+                        Print(tree, printMethod);
+                        isContain = tree.DFS(NumberInput(messages[Messages.EnterNumber], VALUE_MIN, VALUE_MAX, false));
+                        MessageWaitKey(isContain ? messages[Messages.Contain] : messages[Messages.NotContain]);
+                        Print(tree, printMethod);
+                        break;
+                    case 4://exit
+                        isExit = true;
+                        break;
+                }
+
+            }
+
+
+
+            return 0;
+
+        }
+
+        private static void AddRandomNumberToTree(BTree tree, Random rnd)
+        {
+            bool isDone = false;
+            while (!isDone)
+            {
+                int newValue = rnd.Next(VALUE_MIN, VALUE_MAX + 1);
+                if (!tree.Contains(newValue))
+                {
+                    tree.AddNode(newValue);
+                    isDone = true;
+                }
+
+            }
+        }
+
+        private static void Print(BTree tree, bool method = false)
+        {
+            Console.Clear();
+            if (method)
+            {
+                tree.Print();
+            }
+            else
+            {
+                BTreePrinter.Print(tree.Root, true);
+            }
+        }
+
+
+        #region -------- Вспомогательные методы --------
+
+        /// <summary>
+        /// Метод запрашивает у пользователя целое int число.
+        /// </summary>
+        /// <param name="message">Сообщение для пользователя</param>
+        /// <param name="min">Минимальное значение ввода</param>
+        /// <param name="max">Максимальное значение ввода</param>
+        /// <param name="isOneDigit">Запрашивать одну цифру или несколько</param>
+        /// <returns>Введенное пользователем целое число больше нуля.</returns>
+        private static int NumberInput(string message, int min, int max, bool isOneDigit = true)
+        {
+            bool isInputCorrect = false; //флаг проверки
+            int input = 0;
+            Console.WriteLine($"{message}({messages[Messages.From]} {min} {messages[Messages.To]} {max})");
+            while (!isInputCorrect) //Цикл будет повторятся, пока вводимое число не пройдет все проверки
+            {
+                if (isOneDigit)
+                    isInputCorrect = int.TryParse(Console.ReadKey().KeyChar.ToString(), out input);
+                else
+                    isInputCorrect = int.TryParse(Console.ReadLine(), out input);
+
+                if (isInputCorrect && (input < min || input > max))
+                    isInputCorrect = false;
+
+                if (!isInputCorrect)
+                    if (isOneDigit)
+                        try
+                        {
+                            Console.CursorLeft--;//Если ввели что-то не то, то просто возвращаем курсор на прежнее место
+                        }
+                        catch
+                        {
+                            //В случае ошибки, ввода каких-либо управляющих символов или попытках выхода курсора
+                            //за пределы консоли, просто ничего не делаем и остаемся на месте
+                        }
+                    else
+                    {
+                        Console.WriteLine(errors[Errors.RepeatInputError]);
+                        try
+                        {
+                            Console.CursorLeft = 0;//Если ввели что-то не то, то просто возвращаем курсор на прежнее место
+                            Console.CursorTop -= 2;
+                            Console.Write(messages[Messages.WhiteSpaceLine]);
+                            Console.CursorLeft = 0;
+                        }
+                        catch
+                        {
+                            //В случае ошибки, ввода каких-либо управляющих символов или попытках выхода курсора
+                            //за пределы консоли, просто ничего не делаем и остаемся на месте
+                        }
+                    }
+            }
+            Console.WriteLine();
+            return input;
+        }
+
+
+        /// <summary> Выводит на экран сообщение и ждет нажатия любой клавиши </summary>
+        /// <param name="message">Сообщение для пользователя</param>
+        private static void MessageWaitKey(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(messages[Messages.PressAnyKey]);
+            Console.ReadKey();
+        }
+        #endregion
+    }
+}
